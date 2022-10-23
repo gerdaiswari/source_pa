5d8100f88fe98cc07fdc601373d8d687d919a691
Gerda Iswari <64198931+gerdaiswari@users.noreply.github.com>
2022-10-23 17:29:54 +0700

    Update start3.sh

--- scripts/start3.sh
+++ scripts/start3.sh
@@ -2,6 +2,5 @@
 
 ip44="$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
 cd /var/www/html
-php //composer.phar update
-php //composer.phar install
+
 php artisan serve --host=$ip44 --port=9455 & php artisan storage:link
