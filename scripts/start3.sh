6860cbb6d52ff989e8773b1a608dc93f72fa9aa4
Gerda Iswari <64198931+gerdaiswari@users.noreply.github.com>
2022-10-23 15:25:32 +0700

    Update start3.sh

--- scripts/start3.sh
+++ scripts/start3.sh
@@ -2,4 +2,4 @@
 
 ip44="$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
 cd /var/www/html
-php7 artisan serve --host=$ip44 --port=80 & php7 artisan storage:link
+php artisan serve --host=$ip44 --port=80 & php artisan storage:link
